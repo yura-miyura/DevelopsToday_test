28601fb3f9312e8d7b193421d5157c93d48c2c9a
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2026-04-13 16:11:44 +0300

    external api service implemented

---
+++ services.py
@@ -0,0 +1,11 @@
+import httpx
+from fastapi import HTTPException
+
+async def validate_artwork_id(external_id: int):
+    """Checks if the artwork ID exists in the Art Institute of Chicago API."""
+    url = f"https://api.artic.edu/api/v1/artworks/{external_id}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        if response.status_code != 200:
+            raise HTTPException(status_code=400, detail=f"Place ID {external_id} not found in Art Institute API")
+        return response.json()
