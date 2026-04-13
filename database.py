60dd888d68993bc2bf1b74fcd909d7879117f113
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2026-04-13 16:10:09 +0300

    database engine created

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./travel_plans.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
